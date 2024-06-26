d466f54fd844444500a06e90b73db33debb8e04c
Nota Name <email@thisisnotanemail.com>
2024-06-26 16:15:46 -0700

    inint

---
+++ script.js
@@ -0,0 +1,32 @@
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    parent: 'game-container',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+const game = new Phaser.Game(config);
+
+function preload() {
+    // Load assets here
+}
+
+function create() {
+    // Set up game objects here
+}
+
+function update() {
+    // Game logic goes here
+}
