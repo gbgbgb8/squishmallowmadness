896171827092738a9e9ff7773eccc9cf8ed01007
Nota Name <email@thisisnotanemail.com>
2024-06-27 07:16:50 -0700

    716

--- script.js
+++ script.js
@@ -49,9 +49,9 @@ function create() {
     createPlatform(400, 568, 8, 1);
     
     // Create other platforms
-    createPlatform(600, 400, 2, 1);
-    createPlatform(50, 250, 2, 1);
-    createPlatform(750, 220, 2, 1);
+    createPlatform(200, 400, 4, 1);  // Left platform
+    createPlatform(600, 250, 4, 1);  // Right platform
+    createPlatform(400, 100, 3, 1);  // Top center platform
 
     player = this.physics.add.sprite(100, 450, 'hero');
     player.setDisplaySize(60, 60);
